0addb1ab8fe037a80ef4c9402dc054e551ed59d5
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-05-14 18:18:45 +0200

    changed chart colors

--- source/javascripts/chart.js
+++ source/javascripts/chart.js
@@ -15,6 +15,8 @@ options = {
     subtitle: {
         text: ''
     },
+    colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', 
+       '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92'],
     xAxis: {
         type: 'datetime',
         dateTimeLabelFormats: {
